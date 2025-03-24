625895d02c7eeaf84ac8b1fcd27c14bee1f64fa0
shameer <shameerrr03@gmail.com>
2025-03-24 07:13:45 +0500

    troubleshooting

--- app/config.py
+++ app/config.py
@@ -1,19 +1,22 @@
+import os
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    database_hostname: str
-    database_port: str
-    database_password: str
-    database_name: str
-    database_username: str
-    secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
+    database_hostname: str = os.environ.get("DATABASE_HOSTNAME")
+    database_port: str = os.environ.get("DATABASE_PORT")
+    database_password: str = os.environ.get("DATABASE_PASSWORD")
+    database_name: str = os.environ.get("DATABASE_NAME")
+    database_username: str = os.environ.get("DATABASE_USERNAME")
+    secret_key: str = os.environ.get("SECRET_KEY")
+    algorithm: str = os.environ.get("ALGORITHM")
+    access_token_expire_minutes: int = int(
+        os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES")
+    )  # convert to int
 
-    # this is going to import variables from our .env file
-    class Config:
-        env_file = ".env"
+
+class Config:
+    env_file = ".env"
 
 
 settings = Settings()
